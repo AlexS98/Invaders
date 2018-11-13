44612e916aef3be5a00ba7c8dc1d1d7d53b9a1dc
Vladislav1621 <Vladislav1621@mail.ru>
2018-11-13 02:48:38 +0200

    add simple test

--- Tests/Test.cs
+++ Tests/Test.cs
@@ -7,13 +7,22 @@ namespace Tests
 {
     public class Test
     {
-            [Fact]
-            public void PlayerTest()
-            {
-                Player p = new Player(true, "", 5, new GameResources(10, 10, 10));
-                Assert.Equal(10, p.PlayerResources.Gold);
-                Assert.Equal(10, p.PlayerResources.Wheat);
-                Assert.Equal(10, p.PlayerResources.Wood);
-            }
+        [Fact]
+        public void PlayerTest()
+        {
+            Player p = new Player(true, "", 5, new GameResources(10, 10, 10));
+            Assert.Equal(10, p.PlayerResources.Gold);
+            Assert.Equal(10, p.PlayerResources.Wheat);
+            Assert.Equal(10, p.PlayerResources.Wood);
+        }
+
+        [Fact]
+        public void ResourcesTest()
+        {
+            var res = new GameResources();
+            Assert.Equal(0, res.Gold);
+            Assert.Equal(0, res.Wheat);
+            Assert.Equal(0, res.Wood);
+        }
     }
 }
